7f6b7462af638e2f0a76e5c8526b698cf1d85884
Loïc Juillerat <l.juillerat@avintis.com>
2024-07-05 15:14:35 +0200

    log refactoring

--- scripts/check-deployment-type.cs
+++ scripts/check-deployment-type.cs
@@ -9,7 +9,11 @@ for (i = 0; i < nodes.length; i++){
     type = nodes[i].engineType || (nodes[i].activeEngine || {}).type;
     addon = type ? (type == 'java' ? 'maven' : 'other') : 'mount'
 
-    jelastic.marketplace.console.WriteLog("Retrieved type: " + addon);
+    logResult = jelastic.marketplace.console.WriteLog("Retrieved type: " + addon, false);
+    if (logResult.result != 0) {
+        // Log an error message if logging fails
+        jelastic.marketplace.console.WriteLog("Error logging message: " + logResult.error, true);
+    }
       
     if (addon == 'mount') return {result:99, error: 'deploy to custom containers is not implemented yet', type: 'warning'}
     if (addon == 'maven') envName += '-git-push-${fn.random(1000)}'
