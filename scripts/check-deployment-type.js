5d30a6cf603ca544207eb5b519e89033e329a1ba
Loïc Juillerat <l.juillerat@avintis.com>
2024-07-09 14:29:07 +0200

    testings

--- scripts/check-deployment-type.js
+++ scripts/check-deployment-type.js
@@ -1,18 +1,13 @@
 //@req(next)
 
 var envName = '${env.envName}';
-var session = '${session}';
-var nodes = api.env.control.GetEnvInfo(envName, session).nodes;
+var nodes = jelastic.env.control.GetEnvInfo(envName, session).nodes;
 var addon = 'unknown';
 
 for (var i = 0; i < nodes.length; i++) {
-    api.marketplace.console.WriteLog("Retrieved nodeGroup: " + nodes[i].nodeGroup);
-    
     if (nodes[i].nodeGroup == 'cp') {
         var type = nodes[i].engineType || (nodes[i].activeEngine || {}).type;
-        addon = type ? (type == 'java' ? 'maven' : 'other') : 'mount';
-
-        api.marketplace.console.WriteLog("Retrieved type: " + addon);
+        addon = type ? (type == 'java' ? 'maven' : 'vcs') : 'mount';
         
         if (addon == 'mount') {
             return {
@@ -21,14 +16,20 @@ for (var i = 0; i < nodes.length; i++) {
                 type: 'warning'
             };
         }
-        
         if (addon == 'maven') {
-            envName += '-git-push-${fn.random(1000)}';
+            envName += '-git-push-' + Math.floor(Math.random() * 1000);
         }
-        
-        var resp = { result: 0, onAfterReturn: [] };
+
+        var resp = {
+            result: 0,
+            onAfterReturn: []
+        };
         var o = {};
-        o[next] = { addon: addon, type: type, envName: envName };
+        o[next] = {
+            addon: addon,
+            type: type,
+            envName: envName
+        };
         resp.onAfterReturn.push(o);
 
         return resp;
